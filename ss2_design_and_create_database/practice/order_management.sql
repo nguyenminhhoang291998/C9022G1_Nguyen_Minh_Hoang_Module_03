6d2a7eff90fb4201d1e56169ec1ae0c2c9fe64b5
unknown <you@example.com>
2022-12-15 15:52:07 +0700

    ss2

--- ss2_design_and_create_database/practice/order_management.sql
+++ ss2_design_and_create_database/practice/order_management.sql
@@ -2,68 +2,65 @@ create database order_management;
 use order_management;
 
 create table nha_cc(
-	ma_ncc int primary key auto_increment,
-	ten_ncc varchar(30),
-	dia_chi varchar(30),
-	sdt varchar(10)
+  ma_ncc int primary key auto_increment, 
+  ten_ncc varchar(30), 
+  dia_chi varchar(30), 
+  sdt varchar(10)
 );
 
 create table sdt(
-	sdt varchar(10) ,
-	ma_ncc int,
-	primary key(sdt,ma_ncc),
-	foreign key (ma_ncc) references nha_cc(ma_ncc)
+  sdt varchar(10), 
+  ma_ncc int, 
+  primary key(sdt, ma_ncc), 
+  foreign key (ma_ncc) references nha_cc(ma_ncc)
 );
 
-
-
 create table don_dh(
-	so_hd int primary key auto_increment,
-	ngay_hd date,
-	cung_cap int,
-	foreign key (cung_cap)
-	references nha_cc(ma_ncc)
+  so_hd int primary key auto_increment, 
+  ngay_hd date, 
+  cung_cap int, 
+  foreign key (cung_cap) references nha_cc(ma_ncc)
 );
 
 create table vat_tu(
-	ma_vt int primary key auto_increment,
-	ten_vt varchar(30)
+  ma_vt int primary key auto_increment, 
+  ten_vt varchar(30)
 );
 
 create table chi_tiet_don_dat_hang(
-	so_hd int,
-	ma_vt int,
-	primary key(so_hd,ma_vt),
-	foreign key (ma_vt) references vat_tu(ma_vt),
-	foreign key (so_hd) references don_dh(so_hd)
+  so_hd int, 
+  ma_vt int, 
+  primary key(so_hd, ma_vt), 
+  foreign key (ma_vt) references vat_tu(ma_vt), 
+  foreign key (so_hd) references don_dh(so_hd)
 );
 
 create table phieu_nhap(
-	so_pn int  primary key auto_increment,
-	ngay_nhap date	
+  so_pn int primary key auto_increment, 
+  ngay_nhap date
 );
 
 create table chi_tiet_phieu_nhap(
-	dg_nhap int,
-	sl_nhap int,
-	primary key(dg_nhap,sl_nhap),
-	ma_vt int,
-	so_pn int,
-	foreign key (ma_vt) references vat_tu(ma_vt),
-	foreign key (so_pn) references phieu_nhap(so_pn)
+  dg_nhap int, 
+  sl_nhap int, 
+  primary key(dg_nhap, sl_nhap), 
+  ma_vt int, 
+  so_pn int, 
+  foreign key (ma_vt) references vat_tu(ma_vt), 
+  foreign key (so_pn) references phieu_nhap(so_pn)
 );
 
 create table phieu_xuat(
-	so_px int primary key auto_increment,
-	ngay_xuat date
+  so_px int primary key auto_increment, 
+  ngay_xuat date
 );
 
 create table chi_tiet_phieu_xuat(
-	dg_xuat int,
-	sl_xuat int,
-	 primary key(dg_xuat,sl_xuat),
-	so_px int,
-	ma_vt int,
-	foreign key (so_px) references  phieu_xuat(so_px),
-	foreign key (ma_vt) references  vat_tu(ma_vt)
+  dg_xuat int, 
+  sl_xuat int, 
+  primary key(dg_xuat, sl_xuat), 
+  so_px int, 
+  ma_vt int, 
+  foreign key (so_px) references phieu_xuat(so_px), 
+  foreign key (ma_vt) references vat_tu(ma_vt)
 );
