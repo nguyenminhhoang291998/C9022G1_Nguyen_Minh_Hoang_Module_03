bf75f8c9a3ed28130abc9b8a5b56204cf265c493
nguyenminhhoang291998 <nguyenminhhoang291998@gmail.com>
2022-12-29 16:12:58 +0700

    abc

--- ss9_web/practice/product_discount_calculator/src/main/java/com/example/product_discount_calculator/CalculatorServlet.java
+++ ss9_web/practice/product_discount_calculator/src/main/java/com/example/product_discount_calculator/CalculatorServlet.java
@@ -1,10 +1,11 @@
+package com.example.product_discount_calculator;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "CalculatorServlet", value = "/CalculatorServlet")
+@WebServlet(name = "CalculatorServlet", value = "/display-discount")
 public class CalculatorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -13,6 +14,16 @@ public class CalculatorServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String description = request.getParameter("description");
+        double price = Double.parseDouble(request.getParameter("price"));
+        double percent = Double.parseDouble(request.getParameter("percent"));
+        double discountAmount = price * percent * 0.01;
+        double discountPrice = price - discountAmount;
 
+        request.setAttribute("description", description);
+        request.setAttribute("discountAmount", discountAmount);
+        request.setAttribute("discountPrice", discountPrice);
+        RequestDispatcher rs = request.getRequestDispatcher("result.jsp");
+        rs.forward(request, response);
     }
 }
